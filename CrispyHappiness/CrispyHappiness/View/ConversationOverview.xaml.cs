9ce2d5fb04cd3e6abbc0ac067e4494e728b2f19c
Michal Provazník <rockeater0@gmail.com>
2020-04-10 22:58:20 +0200

    Buttons bee poppin

--- CrispyHappiness/CrispyHappiness/View/ConversationOverview.xaml.cs
+++ CrispyHappiness/CrispyHappiness/View/ConversationOverview.xaml.cs
@@ -12,14 +12,25 @@ namespace CrispyHappiness.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConversationOverview : ContentPage
     {
+        Button button;
+       List<int> IdList = new List<int>();
         public ConversationOverview()
         {
             InitializeComponent();
+            for (int k = 0; k < 5; k++)
+            {
+                IdList.Add(k);
+            }
+            int i = 0;
             foreach (int ID in IdList)
             {
-                int i = 0;
+                button = new Button();
                 conversationGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-                conversationGrid.SetRow(button, i++);
+                conversationGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
+                Grid.SetRow(button, i);
+                Grid.SetColumn(button, 0);
+                i++;
+                conversationGrid.Children.Add(button);
             }
         }
     }
