3e76f4fabb0f053f81c6085139c3ec4b0c8419c9
Maximilien Louis Dal Viallon <maximilien.viallon@gmail.com>
2020-04-11 01:01:05 +0200

    can't remember what I did

--- CrispyHappiness/CrispyHappiness/View/ConversationOverview.xaml.cs
+++ CrispyHappiness/CrispyHappiness/View/ConversationOverview.xaml.cs
@@ -12,19 +12,24 @@ namespace CrispyHappiness.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ConversationOverview : ContentPage
     {
-        Button button;
+       Button button;
        List<int> IdList = new List<int>();
         public ConversationOverview()
         {
             InitializeComponent();
-            for (int k = 0; k < 30; k++)
+            for (int k = 0; k < 15; k++)
             {
                 IdList.Add(k);
             }
             int i = 0;
             foreach (int ID in IdList)
             {
-                button = new Button();
+                button = new Button
+                {
+                    Text = "name",
+
+
+                };
                 conversationGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
                 conversationGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
                 Grid.SetRow(button, i);
