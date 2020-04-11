69d8def650d7ea3fb7f92f298295709f22aef258
Michal Provazník <rockeater0@gmail.com>
2020-04-11 22:08:32 +0200

    That one more thing

--- CrispyHappiness/CrispyHappiness/View/ConversationOverview.xaml.cs
+++ CrispyHappiness/CrispyHappiness/View/ConversationOverview.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrispyHappiness.Webservices;
+using CrispyHappiness.Model;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,16 +15,24 @@ namespace CrispyHappiness.View
     public partial class ConversationOverview : ContentPage
     {
         Button button;
-       List<int> IdList = new List<int>();
+        List<int> IdList = new List<int>();
+        FakeWebService totallyLegitimateWebService = new FakeWebService();
+        List<Conversation> conversations = new List<Conversation>();
         public ConversationOverview()
         {
             InitializeComponent();
+
+            Conversation[] kon = new Conversation[3];
+            TaskCaller();
+            searchBar.Text = "Bamboozle";
+
             for (int k = 0; k < 30; k++)
             {
                 IdList.Add(k);
             }
             int i = 0;
-            foreach (int ID in IdList)
+            searchBar.Text += conversations.Count().ToString();
+            foreach (var item in conversations)
             {
                 button = new Button();
                 Grid dynGrid = new Grid();
@@ -43,7 +53,7 @@ namespace CrispyHappiness.View
 
                 StackLayout dynStackTxt = new StackLayout();
                 Label labelName = new Label();
-                labelName.Text = "name";
+                labelName.Text = item.Username;
                 Label labelLastSext = new Label();
                 labelLastSext.Text = "none";
                 dynStackTxt.Children.Add(labelName);
@@ -56,6 +66,22 @@ namespace CrispyHappiness.View
                 scrollyStack.Children.Add(dynGrid);
                 i++;
             }
+        }
+
+        public void TaskCaller()
+        {
+           var conversations2 = Task.Run(async () => await TaskRetriever()).Result;
+            foreach (var item in conversations2)
+            {
+                conversations.Add(item);
+                searchBar.Text += conversations.Count().ToString();
+            }
+        }
+        public async Task<Conversation[]> TaskRetriever()
+        {
+            var conversations2 = await totallyLegitimateWebService.GetConversations(1);
+            searchBar.Text = conversations2.Count().ToString();
+            return conversations2;
         }
 
         private void ImgButt_Clicked(object sender, EventArgs e)
